22210b84130e60ff56b7b0b2d0b7d195ddd6d4dd
vagrant <vagrant@precise64.(none)>
2013-01-22 21:16:45 +0000

    I\'m a dumbass that cannot close braces

--- booking/app.py
+++ booking/app.py
@@ -3,5 +3,5 @@ from flask import Flask
 app = Flask(__name__)
 
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 8100)
+    port = int(os.environ.get('PORT', 8100))
     app.run(host='0.0.0.0', port=port)
