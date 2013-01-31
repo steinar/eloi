34b22174d9b7f98a5d62e19bcaa38611300c8f1b
Axel Ingolfsson <axel@gagnavarslan.is>
2013-01-31 21:28:34 +0000

    Base for booking slots view

--- booking/views.py
+++ booking/views.py
@@ -1,4 +1,6 @@
 import yaml
+
+from flask import request
 from flask.templating import render_template
 
 from booking.app import app
@@ -7,4 +9,11 @@ from booking.app import app
 @app.route('/')
 def locations():
     data = yaml.load(file('base.yaml', 'r'))
-    return render_template('location.html', locations=data['Locations'])
+    return render_template('location.html', locations=data['Location'])
+
+
+@app.route('/location/<location_slug>/')
+def location(location_slug):
+    weeks = request.args.get('weeks', '')
+    weeks = weeks and weeks.split('-') or []
+    return render_template('slots.html', location_slug=location_slug, weeks=weeks)
