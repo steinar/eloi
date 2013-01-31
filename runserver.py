b2f7115c84e7d9013933d25cbd4b14c9e9f8d97b
Skúli Arnlaugsson <skuli@gagnavarslan.is>
2013-01-31 16:53:14 +0000

    Use SERVER_PORT from settings.py when starting server

--- runserver.py
+++ runserver.py
@@ -1,8 +1,9 @@
 import os
 from booking.app import app
+from booking.settings import SERVER_PORT
 from booking import views
 
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 8100))
+    port = int(os.environ.get('PORT', SERVER_PORT))
     app.debug = True
     app.run(host='0.0.0.0', port=port)
