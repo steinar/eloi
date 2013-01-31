3b85e560595fa971fde1f705b16eecfe3c24df1e
Skúli Arnlaugsson <skuli@gagnavarslan.is>
2013-01-31 18:45:23 +0000

    Changed from static to yaml data

--- booking/views.py
+++ booking/views.py
@@ -1,3 +1,4 @@
+import yaml
 from flask.templating import render_template
 
 from booking.app import app
@@ -5,15 +6,5 @@ from booking.app import app
 
 @app.route('/')
 def locations():
-
-    # Test data, will be replaced by data from the database
-    locations = [{
-        'title': u'Dans og veislusalur',
-        'img': 'http://fbjork.is/thumb.php?file=/files/138-0.jpg&size=200x150',
-        'description': 'Description'
-    }, {
-        'title': u'Dans og veislusalur',
-        'img': 'http://fbjork.is/thumb.php?file=/files/138-0.jpg&size=200x150',
-        'description': 'Description'
-    }]
-    return render_template('location.html', locations=locations)
+    data = yaml.load(file('base.yaml', 'r'))
+    return render_template('location.html', locations=data['Locations'])
